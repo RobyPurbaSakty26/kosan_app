b1a11266401c055b2a0ea4cecb1f8e31e55329d4
RobyPurbaSakty26 <obykao26@gmail.com>
2022-04-15 07:40:41 +0700

    memulia projek app kosan

---
+++ lib/theme.dart
@@ -0,0 +1,31 @@
+// ignore_for_file: prefer_const_constructors, import_of_legacy_library_into_null_safe
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color purpleColor = Color(0xff5843BE);
+Color orangeColor = Color(0xffFF9376);
+Color grayColor = Color(0xff7A7E86);
+Color blackColor = Color(0xff000000);
+Color whiteColor = Color(0xffffffff);
+
+double edge = 24;
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: blackColor,
+);
+
+TextStyle whitekStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: whiteColor,
+);
+
+TextStyle grayStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: grayColor,
+);
+
+TextStyle purplrStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: purpleColor,
+);
